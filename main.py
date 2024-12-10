8af67c167f9eef49d3a0e47645d7fd2f23f8ccff
Ivan Ruchkin <rururuchkin@gmail.com>
2024-12-10 23:45:29 +0300

    First commet

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def get_contours_and_vals(image):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([100, 150, 0])
+    upper_blue = np.array([140, 255, 255])
+    mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    output_image = image.copy()
+    cv2.drawContours(output_image, contours, -1, (0, 128, 255), 2)
+
+    count_of_contours = len(contours)
+
+    return output_image, count_of_contours
+
+image = cv2.imread('template_in_0.jpg')
